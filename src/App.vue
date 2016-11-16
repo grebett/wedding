f9febad89175b8044beb232639109132b048f0fe
Gregory Betton <gregory.betton@blablacar.com>
2016-11-16 17:23:29 +0100

    feat: playing with background render aspects

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,29 @@ html, body {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  background-image: url(./assets/festival-flags-garden-party-large.jpg);
-  background-size: cover;
-  background-position: top;
-  background-attachment: fixed;
   margin: auto;
   width: 100%;
   min-height: 100%;
   height: auto;
+  left: 0;
+  right: 0;
+  z-index: 0;
+}
+
+#app:before {
+  content: "";
+  position: fixed;
+  left: 0;
+  right: 0;
+  z-index: -1;
+
+  display: block;
+  background-image: url(./assets/festival-flags-garden-party-large.jpg);
+  background-size: cover;
+  background-position: top;
+  background-attachment: fixed;
+  width: 1200px;
+  height: 800px;
+  filter: blur(2px) hue-rotate(10deg) brightness(0.7);
 }
 </style>
